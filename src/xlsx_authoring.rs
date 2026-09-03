3d23b117eb5488ff06cd9d627e23f809706efe88
odcpw <odcpw@users.noreply.github.com>
2026-09-03 21:53:14 +0200

    ooxml-epic-e-gu5.4 honor SOURCE_DATE_EPOCH in XLSX scaffolds code-first, batch-verify pending

--- src/xlsx_authoring.rs
+++ src/xlsx_authoring.rs
@@ -139,7 +139,12 @@ fn write_xlsx_scaffold_package(
         "_rels/.rels",
         package_relationships_xml(),
     )?;
-    write_zip_string(&mut writer, options, "docProps/core.xml", core_props_xml())?;
+    write_zip_string(
+        &mut writer,
+        options,
+        "docProps/core.xml",
+        &core_props_xml()?,
+    )?;
     write_zip_string(
         &mut writer,
         options,
@@ -206,8 +211,18 @@ fn package_relationships_xml() -> &'static str {
     r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?><Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships"><Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument" Target="xl/workbook.xml"/><Relationship Id="rId2" Type="http://schemas.openxmlformats.org/package/2006/relationships/metadata/core-properties" Target="docProps/core.xml"/><Relationship Id="rId3" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties" Target="docProps/app.xml"/></Relationships>"#
 }
 
-fn core_props_xml() -> &'static str {
-    r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?><cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:creator>ooxml-cli</dc:creator><cp:lastModifiedBy>ooxml-cli</cp:lastModifiedBy></cp:coreProperties>"#
+fn core_props_xml() -> CliResult<String> {
+    let dates = crate::env_conventions::source_date_epoch_timestamp()?.map_or_else(
+        String::new,
+        |timestamp| {
+            format!(
+                r#"<dcterms:created xsi:type="dcterms:W3CDTF">{timestamp}</dcterms:created><dcterms:modified xsi:type="dcterms:W3CDTF">{timestamp}</dcterms:modified>"#
+            )
+        },
+    );
+    Ok(format!(
+        r#"<?xml version="1.0" encoding="UTF-8" standalone="yes"?><cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><dc:creator>ooxml-cli</dc:creator><cp:lastModifiedBy>ooxml-cli</cp:lastModifiedBy>{dates}</cp:coreProperties>"#
+    ))
 }
 
 fn app_props_xml(sheet_names: &[String]) -> String {
